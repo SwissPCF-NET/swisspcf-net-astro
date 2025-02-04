b52ecf6d84b72ccc14c96d82668b6e924285c4c6
Roman Mylonas <romanoman@gmail.com>
2025-02-04 13:39:41 +0100

    Add objectives.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'REPPSO';
-export const SITE_DESCRIPTION = 'REPPSO';
+export const SITE_TITLE = 'SwissPCF-NET';
+export const SITE_DESCRIPTION = 'SwissPCF-Net federates the academic core facilities that provide proteomics and protein analysis services in Switzerland.';
